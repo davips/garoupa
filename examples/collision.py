d332893e750031bc0fa6df29c6ec2cb5870b62e9
davips <dpsabc@gmail.com>
2020-11-26 22:41:30 -0300

    Better collision test

--- examples/collision.py
+++ examples/collision.py
@@ -1,39 +1,50 @@
 # Check distribution of collisions after multiplication.
-import json
-from math import factorial
-import random
+from timeit import timeit
 
 import fastrand
 import psutil as psutil
 
-from cruipto.linalg import pmat_mult, pmat2int, int2pmat
+# Check collisions. (seems unnecessary, due to the nature of the mult op.
+from cruipto.hash import Hash
+from cruipto.hashmath import int2bm, bmm, bm2int
 
-# To check collisions. (seems unnecessary, due to the nature of the mult op.
-s = {}
-t = {}
-lim = factorial(35) - 1
+s, re = {}, {}
+lim = 2 ** 128 - 1
 m = lim  # factorial(35) - 5000000
 n = lim - 10_000_000  # factorial(35) - 2
-c = 0
+c, t = 0, 0
 for i in range(lim, lim - 10, -1):
-    ii = random.randrange(lim)
-    mi = int2pmat(ii)
+    while True:
+        ii = ((fastrand.xorshift128plus() + 2 ** 63) << 64) + (fastrand.xorshift128plus() + 2 ** 63)
+        if ii not in s:
+            break
+        print("another ii", ii)
+    mi = int2bm(ii)
+    s[ii] = s.get(ii, 0) + 1
+    c += 1
     for j in range(m, n, -1):
-        jj = random.randrange(lim)
+        while True:
+            jj = ((fastrand.xorshift128plus() + 2 ** 63) << 64) + (fastrand.xorshift128plus() + 2 ** 63)
+            if jj not in s:
+                break
+            print("another jj", jj)
         c += 1
-        mj = int2pmat(jj)
-        r = pmat2int(pmat_mult(mi, mj))
-        s[r] = s.get(r, 0) + 1
+        mj = int2bm(jj)
+        r = bm2int(bmm(mi, mj))
+        re[r] = re.get(r, 0) + 1
+        s[jj] = s.get(jj, 0) + 1
+        t += 1
 
         if (j - n) % round((m - n) * 0.005) == 0:
             mem = psutil.virtual_memory()[2]
             print(100 * (j - n) / (m - n), '%   memory used:', mem, '%')
-            print('unicos:', len(s), 'max/min:', max(s.values()),
-                  min(s.values()),
-                  'tests:', c, 'collision rate:', 100 * (c - len(s)) / c, '%')
+            print('unicos:', len(s), 'max/min:', max(s.values()), min(s.values()),
+                  'tests:', t, 'collision rate:', 100 * (t - len(re)) / t, '%')
             print()
             if mem > 96:
                 print('out of memory')
                 break
 
+
 # print(json.dumps(s, indent=4))
+
--- examples/uuidcollisions.py
+++ examples/uuidcollisions.py
@@ -1,13 +1,14 @@
 from random import random
 from timeit import timeit
 
-from cruipto.linalg import int2pmat, print_binmatrix, pmat2int, \
-    int2fac, pmat_mult
+from cruipto.hash import Hash
+from cruipto.linalg import int2pmat, pmat2int, \
+    pmat_mult
 from cruipto.uuid import UUID
 
 # Show output of operations.
 a = UUID(int2pmat(2 ** 128 - 1))
-b = UUID('12345678901234')
+b = UUID('12345678901234123456789')
 c = UUID(1)
 print(a, b, c)
 print()
@@ -17,35 +18,42 @@ print((a * b) * b.t)
 print((a * b) * c)
 
 # Check for collisions.
-s = set()
-r = set()
+s = {0}
 aa = bb = 0
 for i in range(100000):
-    while aa in r:
+    while aa in s:
         aa = round(random() * 2 ** 128)
-    while bb in r:
+    while bb in s:
         bb = round(random() * 2 ** 128)
-    r.add(aa)
-    r.add(bb)
     a = int2pmat(aa)
     b = int2pmat(bb)
     n = pmat2int(pmat_mult(a, b))
-    s.add(a)
-    s.add(b)
+    s.add(aa)
+    s.add(bb)
     s.add(n)
     if i > len(s) - 1:
         print(i, a, b, n)
         print('Colision detected!!!!!!!!!!!!!!')
         break
-if i == 100000-1:
-    print('No collisions!')
+if i == 100000 - 1:
+    print('No collisions!', len(s))
 
 
 # Check general overhead of all uuid ops.
 def f():
     a = UUID(int2pmat(2 ** 128 - 1))
-    b = UUID('12345678901234')
+    b = UUID('12345678901234123456789')
     (a * b) * b.t
 
 
+print(timeit(f, number=10000) * 100, 'us')
+
+
+# Check general overhead of all hash ops.
+def f():
+    a = Hash(2 ** 128 - 1)
+    b = Hash('1234567890123412345678')
+    (a * b) / b
+
+f()
 print(timeit(f, number=10000) * 100, 'us')
