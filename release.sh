f5fce1e3f25bfca1c00f10fa47b7301d60dd477a
davips <dpsabc@gmail.com>
2021-03-31 00:36:14 -0300

    Improve release.sh

--- release.sh
+++ release.sh
@@ -1,32 +1,41 @@
+echo
 pytest src --cov=src --doctest-modules
 echo "----------------- tested -----------------------"
 read -p "press any key"
 
+echo
 autoreadme -i README-edit.md -s examples/ -o README.md
 echo "---------------- readme done ----------------"
 read -p "press any key"
 
+echo
 poetry version patch
 echo "--------------- version bumped --------------"
 
+echo
 git status
 echo "------------------ status -----------------------"
 read -p "press any key"
 
+echo
 git commit -am "Release"
 echo "------------------ commited --------------------"
 read -p "press any key"
 
+echo
 git status
 echo "------------------ new status -----------------------"
 read -p "press any key"
 
+echo
 git tag "v$(poetry version | cut -d' ' -f2)" -m "Release v$(poetry version | cut -d' ' -f2)"
 echo "------------------- tagged ----------------------"
 read -p "press any key"
 
+echo
 git push
 echo "------------------- pushed ----------------------"
 read -p "press any key"
 
+echo
 poetry publish --build
