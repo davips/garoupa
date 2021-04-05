5a2f0b046caedb0b59937f0ee5cd5badc33bc979
davips <dpsabc@gmail.com>
2021-04-05 06:08:06 -0300

    Fix seed

--- examples/repetition.py
+++ examples/repetition.py
@@ -9,18 +9,21 @@ from lange import gp
 from garoupa.algebra.matrix import M
 from garoupa.algebra.matrix.m128bit import M128bit
 
+seed = 0
+
 
 def g(le):
-    print("fork", flush=True)
+    global seed
+    seed += 1
+    print("fork", seed, flush=True)
     # for le in gp[1000, 1500, ...]:
     s = {}
 
     def f():
         for l in range(12, 18):
-            G = M(l)  # if l < 18 else M128bit()
-            print(G, 99999)
+            G = M(l, seed=seed*l)  # if l < 18 else M128bit()
             z = G.identity
-            for j in range(2):
+            for j in range(100):
                 t = 1
                 a = ~G
                 r = a
@@ -28,7 +31,7 @@ def g(le):
                     r *= a
                     t += 1
                     if r == z:
-                        s[str(G)] = t, a.i
+                        s[str(G), t] = a.i
                         break
             print(G, G.cells, t, sep="\t", flush=True)
         pprint(s)
