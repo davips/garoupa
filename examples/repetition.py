1f67e7405371b86be29053db015dfec8c14bf8bb
davips <dpsabc@gmail.com>
2021-04-07 03:37:07 -0300

    4 exps

--- examples/repetition.py
+++ examples/repetition.py
@@ -2,19 +2,40 @@
 import operator
 from datetime import datetime
 from functools import reduce
+from itertools import chain, combinations
 from math import log
+from sys import argv
 
 from garoupa.algebra.dihedral import D
-from garoupa.algebra.symmetric import S
+
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+ps = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+      113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239,
+      241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
+      383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521,
+      523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661,
+      673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827,
+      829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991,
+      997, 1009]
 
 limit = 100_000_000
 step = 1
 sample = 100_000_000_000
-Ds = reduce(operator.mul, [D(n) for n in range(5, 49, 2)])
-Ss = reduce(operator.mul, [S(n) for n in range(3, 5, 1)])
-# G = Ds * Ss
-G = reduce(operator.mul, [D(2**i) for i in range(6, 28)])
-print(f"bits: {log(G.order, 2):.2f}\tPc: {G.comm_degree or -1:.2e}", G)
+if argv[1] == "p128":
+    G = reduce(operator.mul, [D(n) for n in ps[:22]])
+elif argv[1] == "p256":
+    G = reduce(operator.mul, [D(n) for n in ps[:38]])
+elif argv[1] == "128":
+    G = reduce(operator.mul, [D(n) for n in range(5, 51, 2)])
+else:
+    G = reduce(operator.mul, [D(n) for n in range(5, 86, 2)])
+
+print(G.bits, G.comm_degree)
 print("--------------------------------------------------------------")
 for hist in G.sampled_orders(sample=sample, limit=limit):
     bad = sum(v for k, v in hist.items() if k[0] < limit)
