baed4c5624612558f261d5c20e065d6d0869ec96
davips <dpsabc@gmail.com>
2021-08-10 22:33:59 -0300

    Release

--- release.sh
+++ release.sh
@@ -1,19 +1,24 @@
+poetry update
+poetry install
+read -p "press enter"
+echo
+
 echo
 echo "----------------- test -----------------------"
-pip install numpy pathos progress
-pytest src tests --cov=src --doctest-modules
+poetry run pytest src tests --cov=src --doctest-modules
 echo "----------------- tested -----------------------"
 echo; echo
 
 echo "---------------- readme ----------------"
-read -p "press any key"
+read -p "press enter"
+source venv/bin/activate
 autoreadme -i README-edit.md -s examples/ -o README.md
-pip uninstall numpy pathos progress
+deactivate
 echo "---------------- readme done ----------------"
 echo; echo
 
 echo "--------------- version bump --------------"
-read -p "press any key"
+read -p "press enter"
 poetry version patch
 echo "--------------- version bumped --------------"
 echo; echo
@@ -24,29 +29,29 @@ echo "------------------ current status shown-----------------"
 echo; echo
 
 echo "------------------ commit --------------------"
-read -p "press any key"
+read -p "press enter"
 git commit -am "Release"
 echo "------------------ commited --------------------"
 echo; echo
 
 echo "------------------ new status... -----------------------"
-read -p "press any key"
+read -p "press enter"
 git status
 echo "------------------ new status shown --------------------"
 echo; echo
 
 echo "------------------- tag ----------------------"
-read -p "press any key"
+read -p "press enter"
 git tag "v$(poetry version | cut -d' ' -f2)" -m "Release v$(poetry version | cut -d' ' -f2)"
 echo "------------------- tagged ----------------------"
 echo; echo
 
 echo "------------------- push ----------------------"
-read -p "press any key"
+read -p "press enter"
 git push
 echo "------------------- pushed ----------------------"
 echo; echo
 
 echo "------------------- publish ----------------------"
-read -p "press any key"
+read -p "press enter"
 poetry publish --build
