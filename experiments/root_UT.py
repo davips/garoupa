d432efd06d8641542cc22af302112e96720085ac
davips <dpsabc@gmail.com>
2021-12-28 21:50:57 -0300

    Improve root experiment, but still bad for big numbers due to numpy types

--- experiments/root_UT.py
+++ experiments/root_UT.py
@@ -1,27 +1,29 @@
+from functools import reduce
+
 import numpy as np
 
 
-def recursive_coef1(k:int)->int:
-    if k==2:
+def recursive_coef1(k: int) -> int:
+    if k == 2:
         return 1
     else:
-        return recursive_coef1(k-1) + (k-1)
+        return recursive_coef1(k - 1) + (k - 1)
 
 
-def recursive_coef2(k:int)->int:
-    if k==2:
+def recursive_coef2(k: int) -> int:
+    if k == 2:
         return 0
     else:
-        return recursive_coef1(k-1) + recursive_coef2(k-1)
+        return recursive_coef1(k - 1) + recursive_coef2(k - 1)
 
 
-def kth_root(x:np.array, p:int, k:int)->np.array:
+def kth_root(x: np.array, p: int, k: int) -> np.array:
     # x: matrix in UT(4, p)
     # p: prime number
     # k: integer s.t. 2 <= k <= p-1
     # returns the kth root of x
 
-    y = np.eye(4)
+    y = np.eye(4, dtype=np.uint64)
 
     divk = pow(k, -1, p)
     C1 = recursive_coef1(k)
@@ -30,7 +32,7 @@ def kth_root(x:np.array, p:int, k:int)->np.array:
     y[0, 1] = (x[0, 1] * divk) % p
     y[1, 2] = (x[1, 2] * divk) % p
     y[2, 3] = (x[2, 3] * divk) % p
-    
+
     y[0, 2] = ((x[0, 2] - C1 * y[0, 1] * y[1, 2]) * divk) % p
     y[1, 3] = ((x[1, 3] - C1 * y[1, 2] * y[2, 3]) * divk) % p
 
@@ -39,25 +41,31 @@ def kth_root(x:np.array, p:int, k:int)->np.array:
     return y
 
 ###########################################################################
-# x in UT(4, 11)
-# computes y such that y^7 = x
+# x in UT(4, p)
+# computes y such that y^k = x
 
-p = 11
-k = 7
+p = 1099511627689
+# p = 1097
+k = 22
 
-x = np.array(
-    [
-        [1., 2., 3., 4.],
-        [0., 1., 5., 6.],
-        [0., 0., 1., 7.],
-        [0., 0., 0., 1.]
-    ]
+x = np.array([
+    [1, 2, 3, 4],
+    [0, 1, 5, 6],
+    [0, 0, 1, 7],
+    [0, 0, 0, 1]], dtype=np.uint64
 )
 
 y = kth_root(x, p, k)
-print(f"x \n = \n{x}")
-print(f"y \n = \n{y}")
-print(f"y^k \n = \n{np.linalg.matrix_power(y, k) % p}")
+print(f"\nx = \n{x}")
+print(f"\ny = \n{y}")
 
 
+def m(a, b):
+    return (a @ b) % p
 
+
+y7 = reduce(m, [y] * k)
+print(f"\ny^{k} = \n{y7}")
+print((y7 == x).all())
+
+# print(timeit(lambda: kth_root(x, p, k), number=1000), "ms")
